3e8e4499f944e9cd8ef4a1eb29b72d27db460845
Jhoselin88 <jhosecolque30@gmail.com>
2026-05-14 13:57:34 -0400

    Implementar clienteCln

---
+++ Sis457Restaurante/ClnRestaurante/ClienteCln.cs
@@ -0,0 +1,64 @@
+﻿using CadRestaurante;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClnRestaurante
+{
+    public class ClienteCln
+    {
+        public static int crear(Cliente cliente)
+        {
+            using (var context = new LabRestauranteEntities())
+            {
+                context.Cliente.Add(cliente);
+                context.SaveChanges();
+                return cliente.id;
+            }
+        }
+        public static int actualizar(Cliente cliente)
+        {
+            using (var context = new LabRestauranteEntities())
+            {
+                var existente = context.Cliente.Find(cliente.id);
+                existente.nitId = cliente.nitId;
+                existente.razonSocial = cliente.razonSocial;
+                existente.usuarioRegistro = cliente.usuarioRegistro;
+                return context.SaveChanges();
+            }
+        }
+        public static int eliminar(int id, string usuario)
+        {
+            using (var context = new LabRestauranteEntities())
+            {
+                var cliente = context.Cliente.Find(id);
+                cliente.estado = -1;
+                cliente.usuarioRegistro = usuario;
+                return context.SaveChanges();
+            }
+        }
+        public static Cliente obtenerUno(int id)
+        {
+            using (var context = new LabRestauranteEntities())
+            {
+                return context.Cliente.Find(id);
+            }
+        }
+        public static List<Cliente> listar()
+        {
+            using (var context = new LabRestauranteEntities())
+            {
+                return context.Cliente.Where(x => x.estado != -1).ToList();
+            }
+        }
+        public static List<paClienteListar_Result> listarPa(string parametro)
+        {
+            using (var context = new LabRestauranteEntities())
+            {
+                return context.paClienteListar(parametro).ToList();
+            }
+        }
+    }
+}
