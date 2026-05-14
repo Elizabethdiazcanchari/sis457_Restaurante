867e44b60a9c462648d8d21f6b370d0482bf4e56
Elizabethdiazcanchari <elizabethdiazcanchari@gmail.com>
2026-05-14 14:09:34 -0400

    Implementar DetalleVentaCln

---
+++ Sis457Restaurante/ClnRestaurante/DetalleVentaCln.cs
@@ -0,0 +1,38 @@
+﻿using CadRestaurante;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClnRestaurante
+{
+    public class DetalleVentaCln
+    {
+        public static int crear(DetalleVenta detalle)
+        {
+            using (var context = new LabRestauranteEntities())
+            {
+                context.DetalleVenta.Add(detalle);
+                context.SaveChanges();
+                return detalle.id;
+            }
+        }
+
+        public static List<DetalleVenta> listarPorPedido(int idVenta)
+        {
+            using (var context = new LabRestauranteEntities())
+            {
+                return context.DetalleVenta.Where(x => x.idVenta == idVenta && x.estado != -1).ToList();
+            }
+        }
+
+        public static List<paDetalleVentaListar_Result> listarPa(string parametro)
+        {
+            using (var context = new LabRestauranteEntities())
+            {
+                return context.paDetalleVentaListar(parametro).ToList();
+            }
+        }
+    }
+}
